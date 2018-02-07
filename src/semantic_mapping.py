d61585badcb03519bb5178606d3cd065a25f1ebf
Scott Werwath <swerwath@berkeley.edu>
2018-02-06 22:56:18 -0800

    Added non-regex semantic mapping

---
+++ src/semantic_mapping.py
@@ -0,0 +1,41 @@
+from sklearn.base import TransformerMixin
+from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
+import pickle
+
+def read_replacements(replacement_file_path):
+    return pickle.load(open(replacement_file_path, 'rb'))
+
+class SemanticMapper(TransformerMixin):
+    def __init__(self, replacements, regex=False):
+        self.replacements = replacements
+        self.regex = regex
+
+    def transform(self, labeled_report, *_):
+        result = []
+        for report in labeled_report:
+            new_sentences = []
+            for sentence in report[0]:
+                for r in self.replacements:
+                    if self.regex:
+                        print("Unsupported")
+                    else:
+                        sentence = sentence.replace(" " + r[0] + " ", " " + r[1] + " ")
+                new_sentences.append(sentence)
+            result.append((new_sentences, report[1]))
+        return result
+
+
+
+replacement_path = "data/processed/clever_replacements"
+replacements = read_replacements(replacement_path)
+print(replacements[0])
+
+data = pickle.load(open('src/preproc.txt', 'rb'))
+print(data[0])
+
+mapper = SemanticMapper(replacements)
+new_data = mapper.transform(data)
+
+print(new_data[4])
+print(new_data[5])
+print(new_data[6])
