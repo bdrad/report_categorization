7b25403e7e4b4adaa38743444f1d09e0942abf05
Scott Werwath <swerwath@berkeley.edu>
2018-02-08 18:27:43 -0800

    Gave preprocessing ability to process multiple scripts

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -72,13 +72,18 @@ class ExtraneousSentenceRemover(TransformerMixin):
 
 import argparse
 import pickle
+import itertools
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess a corpus and output it to a file')
-    parser.add_argument('in_path')
-    parser.add_argument('out_path')
+    parser.add_argument('-i','--in_path', nargs='+', required=True)
+    parser.add_argument('-o','--out_path', nargs=1, required=True)
+
     args = parser.parse_args()
 
-    data = get_reports_from_csv(args.in_path)
+    data = [get_reports_from_csv(ip) for ip in args.in_path]
+    merged_data = list(itertools.chain.from_iterable(data))
     pipeline = make_pipeline(ImpressionExtractor(), SentenceTokenizer(), ExtraneousSentenceRemover(), ReportLabeler(), None)
-    preprocessed = pipeline.transform(data)
-    pickle.dump(preprocessed, open(args.out_path, "wb"))
+    preprocessed = pipeline.transform(merged_data)
+    print("Writing " + str(len(preprocessed)) + " preprocessed reports")
+    pickle.dump(preprocessed, open(args.out_path[0], "wb"))
--- src/semantic_mapping.py
+++ src/semantic_mapping.py
@@ -6,6 +6,11 @@ import re
 def read_replacements(replacement_file_path):
     return pickle.load(open(replacement_file_path, 'rb'))
 
+def reports_to_corpus(reports, out_file):
+    for report in report:
+        for sentence in report[0]:
+            out_file.write(sentence + "\n")
+
 class SemanticMapper(TransformerMixin):
     def __init__(self, replacements, regex=False):
         self.replacements = replacements
@@ -27,3 +32,22 @@ class SemanticMapper(TransformerMixin):
 
 DateTimeMapper = SemanticMapper([(r'[0-9][0-9]? [0-9][0-9]? [0-9][0-9][0-9][0-9]', 'DATE'),
                                  (r'[0-9][0-9]? [0-9][0-9] (am|pm)?', 'TIME')], regex=True)
+
+import argparse
+import pickle
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Preprocess a corpus and output it to a file')
+    parser.add_argument('labeled_reports_in_path')
+    parser.add_argument('replacement_file_path')
+    parser.add_argument('corpus_out_path')
+    parser.add_argument('labels_out_path')
+    args = parser.parse_args()
+
+    labeled_reports = pickle.load(open(args.labeled_reports_in_path, "rb"))
+    replacements = read_replacements(args.replacement_file_path)
+    ReplacementMapper = SemanticMapper(replacements)
+    pipeline = make_pipeline(ReplacementMapper, DateTimeMapper, None)
+    labeled_output = pipeline.transform(labeled_reports)
+
+    reports_to_corpus(labeled_output, open(args.corpus_out_path, "w"))
+    pickle.dump(labeled_output, open(args.labels_out_path, "wb"))
