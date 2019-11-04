f8121c6d4a5e722d4d6e90d6c3d341a1745849c6
maxyuan85 <maxyuan85@berkeley.edu>
2019-11-03 19:43:58 -0800

    modified transformers. added spellcheck. also created experimental pipeline

--- preprocessing/transformers.py
+++ preprocessing/transformers.py
@@ -3,16 +3,32 @@ from sklearn.pipeline import Pipeline, FeatureUnion, make_pipeline
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from gensim.models import Phrases
+from spellchecker import SpellChecker
 import itertools
 import pickle
 import re
+
+
+"""
+Preprocessing Classes
+
+inputs:
+list of reports
+    list of observations (stirng)
+
+outputs:
+list of reports
+    list of observations (string)
+
+"""
 
 
 class LowerCaseSentence(TransformerMixin):
     def transform(self, labeled_reports, *_):
         for report_index, report in enumerate(labeled_reports):
             for sent_index, sentence in enumerate(report):
-                labeled_reports[report_index][sent_index] = sentence.lower()
+                labeled_reports[report_index][sent_index] = " ".join(
+                    sentence.lower().split())
 
         return labeled_reports
 
@@ -69,4 +85,30 @@ class SemanticMapper(TransformerMixin):
                     sentence = sentence[:-1]
                 new_sentences.append(sentence)
             result.append(new_sentences)
+        return result
+
+
+class SpellCheckTransformer(TransformerMixin):
+    """
+    This should be in the first step of the pipeline. Should spell check the inputs to a huge dictionary of all words we allow
+
+    """
+
+    def __init__(self, additional_vocab=None):
+        self.spellchecker = SpellChecker()
+        if additional_vocab:
+            self.spellchecker.loadwords(additional_vocab)
+
+    def transform(self, labeled_reports, *_):
+        result = []
+        for report in labeled_reports:
+            new_sentences = []
+            for sentence in report:
+                words = word_tokenize(sentence)
+                filtered_sentence = [
+                    self.spellchecker.correction(w) for w in words]
+                new_sentences.append(" ".join(filtered_sentence))
+
+            result.append(new_sentences)
+
         return result
